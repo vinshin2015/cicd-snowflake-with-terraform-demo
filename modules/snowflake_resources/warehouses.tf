4b93dfc7d432b8ccc379abc9e2c93a3885331408
Vinayak Shinde <vinayak.shinde@gmail.com>
2025-12-03 18:35:53 +0530

    changed the names for demo table and wh

--- modules/snowflake_resources/warehouses.tf
+++ modules/snowflake_resources/warehouses.tf
@@ -1,5 +1,5 @@
-resource "snowflake_warehouse" "task_warehouse_1" {
-  name           = var.env_name == "PROD" ? "VHOL_TASK_WAREHOUSE" : "VHOL_TASK_WAREHOUSE_${var.env_name}"
+resource "snowflake_warehouse" "task_warehouse" {
+  name           = var.env_name == "PROD" ? "VHOL_TASK_WAREHOUSE" : "VHOL_TASK_WAREHOUSE_${var.env_name}_1"
   warehouse_size = var.env_name == "PROD" ? "LARGE" : "SMALL"
   auto_resume    = true
   auto_suspend   = 1
