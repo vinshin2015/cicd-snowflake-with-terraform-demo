deb6803a51ca3f2aaf086b4d6ec8a5a9bd0f9489
Vinayak Shinde <vinayak.shinde@gmail.com>
2025-12-03 18:46:50 +0530

    corrections

--- modules/snowflake_resources/table.tf
+++ modules/snowflake_resources/table.tf
@@ -1,4 +1,4 @@
-resource "snowflake_table" "demo_table_1" {
+resource "snowflake_table" "demo_table" {
   database   = snowflake_database.tf_demo_database.name
   schema     = snowflake_schema.tf_demo_schema.name
   depends_on = [snowflake_grant_privileges_to_account_role.future_table_ro_grant]
--- modules/snowflake_resources/warehouses.tf
+++ modules/snowflake_resources/warehouses.tf
@@ -1,5 +1,5 @@
 resource "snowflake_warehouse" "task_warehouse" {
-  name           = var.env_name == "PROD" ? "VHOL_TASK_WAREHOUSE" : "VHOL_TASK_WAREHOUSE_${var.env_name}_1"
+  name           = var.env_name == "PROD" ? "VHOL_TASK_WAREHOUSE" : "VHOL_TASK_WAREHOUSE_${var.env_name}"
   warehouse_size = var.env_name == "PROD" ? "LARGE" : "SMALL"
   auto_resume    = true
   auto_suspend   = 1
